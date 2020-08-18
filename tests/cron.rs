c66a315638bfa28c27cfff69467754551473e6ac
mobile-bungalow <>
2020-08-17 17:58:27 -0700

    minor test fixes

--- tests/cron.rs
+++ tests/cron.rs
@@ -58,26 +58,7 @@ fn reschedule_deletion() {
     let ret_v = db.blob_get(k).unwrap().unwrap();
     assert_eq!(&v, &ret_v.as_ref(), "Value was prematurely deleted.");
 
-    std::thread::sleep(Duration::from_millis(160));
-
-    let ret_v = db.blob_get(k).unwrap();
-    assert_eq!(ret_v, None, "Value Was not Deleted.");
-}
-
-#[test]
-fn cancel_deletion() {
-    let db = Arc::new(TempDb::new());
-    let mut sched = JobScheduler::new(db.clone()).unwrap();
-
-    let (k, v) = (b"key_1", b"val_1");
-    db.blob_set(k, v.into()).unwrap();
-
-    // the key was inserted
-    let ret_v = db.blob_get(k).unwrap().unwrap();
-    assert_eq!(&v, &ret_v.as_ref(), "Value was not inserted.");
-
-    let dur = Duration::from_millis(250);
-    sched.expire_blob_in(k.into(), dur).unwrap();
+    std::thread::sleep(Duration::from_millis(250));
 
     let ret_v = db.blob_get(k).unwrap();
     assert_eq!(ret_v, None, "Value Was not Deleted.");
